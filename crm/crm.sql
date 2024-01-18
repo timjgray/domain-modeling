0affd163165a09af2a586a6c053f9a25d6485b3e
Tim Gray <tim.gray@kellogg.northwestern.edu>
2024-01-18 03:30:05 +0000

    Class 3 SQL Labs

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,31 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT,
+    email TEXT,
+    address TEXT,
+    phone TEXT,
+    company_id INTEGER, 
+    activity_id INTEGER
+); 
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT 
+); 
+
+CREATE TABLE activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    contact_id INTEGER,
+    employee_id INTEGER,
+    action TEXT,
+    date TEXT
+); 
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT, 
+    email TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,27 @@
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    time INTEGER,
+    image TEXT, 
+    like_id INTEGER,
+    comment_id INTEGER
+); 
 
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT, 
+    email TEXT 
+); 
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER, 
+    user_id INTEGER 
+); 
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    comment TEXT,
+    post_id INTEGER,
+    user_id INTEGER 
+);
